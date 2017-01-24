7c85fb7c14857c585c2f7b4e10b974ebfd6c6141
Yuri Kasperovich <ykas.gg@gmail.com>
2017-01-24 20:39:42 +0300

    Introduce QCS for HammingDistanceCalculator

--- lib/hamming.rb
+++ lib/hamming.rb
@@ -1,24 +1,20 @@
 class HammingDistanceCalculator
   attr_reader :strand1, :strand2
+  attr_reader :hamming_distance
   
   def initialize(strand1, strand2)
-    @strand1          = strand1
-    @strand2          = strand2
+    @strand1 = strand1
+    @strand2 = strand2
     
     @hamming_distance = 0
   end
   
-  def print_hamming_distance
+  def calculate_hamming_distance
     convert_strands_to_characters
     check_and_calculate_hamming_distance    
-    print_result
-    
-    hamming_distance
   end
   
   private
-  
-  attr_reader :hamming_distance
   
   def convert_strands_to_characters
     @strand1 = strand1.chars
@@ -27,22 +23,15 @@ class HammingDistanceCalculator
   
   def check_and_calculate_hamming_distance
     if strand1.length == strand2.length
-      calculate_hamming_distance
+      do_calculate_hamming_distance
     else
       raise "Введено неверное значение"
     end
   end
   
-  def calculate_hamming_distance
+  def do_calculate_hamming_distance
     strand1.each_with_index do |s, index|
       @hamming_distance += 1 if strand1[index] != strand2[index]
     end
   end
-  
-  def print_result
-    puts hamming_distance
-  end
 end
-
-calculator = HammingDistanceCalculator.new("wwwwwwccczzic","eewwwwccczzcc")
-calculator.print_hamming_distance
